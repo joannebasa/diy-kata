12afc5d2de3fddea17d4f1ab8779eab01e0fc6c2
Joanne G. Basa <joannegbasa@gmail.com>
2019-10-22 13:57:27 +0100

    passed humanCatDogYears 4/7 kata 4 - requires more tests

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,35 @@
-const humanCatDogYears = number => {};
+/* eslint-disable prettier/prettier */
+/* Dog years:
+  * 15 years for first human year
+  * additional 9 years for human year 2
+  * additional 5 years for every human year after
 
+   Cat years:
+  * 15 years for first human year
+  * additional 9 years for human year 2
+  * additional 4 years for every human year after
+*/
+const humanCatDogYears = humanYears => {
+    let dogYears, catYears;
+    if(humanYears <= 0){
+      return [0,0,0];
+    }
+  
+    if(humanYears === 1){
+      catYears = 15;
+      dogYears = 15;
+    }
+  
+    if(humanYears === 2){
+      catYears = 15 + 9;
+      dogYears = 15 + 9;
+    }
+  
+    if(humanYears > 2){
+      catYears = (16) + (4 * humanYears);
+      dogYears = (18) + (5 * humanYears);
+    }
+    return [humanYears, catYears, dogYears];
+  };
+  console.log(humanCatDogYears(2));
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,12 @@
+/* eslint-disable prettier/prettier */
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    test("it returns an array of 3 numbers", () => {
+      expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+    });
+
+    test("it returns 15 if humanYear is equal to 1", () => {
+      expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    });
+  });
